625bbc039fc6262c8366d4bd5cc77efd0b24bf69
Marc Syp <marc.syp@gmail.com>
2015-03-25 01:15:23 -0700

    Sensor buttons implemented, stand-in images.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,14 @@
 angular.module('starter.controllers', [])
 
-.controller('ReportCtrl', function($scope) {})
+.controller('ReportCtrl', function($scope) {
+	var imageURLs = ['./img/floorplan1.png', './img/floorplan2.png', './img/floorplan3.png', './img/floorplan4.png', './img/floorplan5.png']
+	$scope.selectedImage = imageURLs[0];
+	$scope.selectSensor = function(index){
+		console.log(index);
+		$scope.selectedImage = imageURLs[index];
+		console.log($scope.selectedImage);	
+	}
+})
 
 .controller('SearchCtrl', function($scope, $stateParams, Chats) {
 })
